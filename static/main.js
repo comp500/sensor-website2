8cdf6e8e859a21c0d1bdf6440f3dc5e41b14ac55
comp500 <comp500@users.noreply.github.com>
2018-10-05 18:45:52 +0100

    Better graphs

--- static/main.js
+++ static/main.js
@@ -1,53 +1,53 @@
-function sendAlert(message) {
-	document.getElementById("alertSection").innerHTML = `
-	<div class="alert alert-warning" role="alert">
-		<strong>Error:</strong> ${message}
-	</div>
-	`;
-}
-
-var currentMeasurementTime = -1;
-window.setInterval(function () {
-	if (currentMeasurementTime > -1) {
-		currentMeasurementTime++;
-		document.getElementById("measurementTime").innerText = currentMeasurementTime;
-	}
-}, 1000);
-
-function updateLive(data) {
-	Object.keys(data).forEach((sensorID) => {
-		if (sensorID == "time") {
-			var date = new Date(data[sensorID]);
-			// Get time difference in seconds
-			var difference = ((Date.now() - date) / 1000).toFixed(0);
-			document.getElementById("measurementTime").innerText = difference;
-			currentMeasurementTime = difference;
-
-			var statusEl = document.getElementById("systemStatus");
-			if (difference > 172800) {
-				statusEl.className = "text-danger";
-				statusEl.innerText = "Broken (greater than two days since last recording)";
-			} else if (difference > 3600) {
-				statusEl.className = "text-warning";
-				statusEl.innerText = "Problematic (greater than one hour since last recording)";
-			} else if (difference > 300) {
-				statusEl.className = "text-info";
-				statusEl.innerText = "Slow (greater than 5 minutes since last recording)";
-			} else {
-				statusEl.className = "text-success";
-				statusEl.innerText = "Fully operational";
-			}
-		} else {
-			var sensorEl = document.getElementById("live-" + sensorID);
-			if (sensorEl) {
-				var decimalPlaces = parseInt(sensorEl.dataset.decimal);
-				sensorEl.innerText = parseFloat(data[sensorID]).toFixed(decimalPlaces);
-			}
-		}
-	});
-}
-
 window.addEventListener("load", function(event) {
+	function sendAlert(message) {
+		document.getElementById("alertSection").innerHTML = `
+		<div class="alert alert-warning" role="alert">
+			<strong>Error:</strong> ${message}
+		</div>
+		`;
+	}
+	
+	var currentMeasurementTime = -1;
+	window.setInterval(function () {
+		if (currentMeasurementTime > -1) {
+			currentMeasurementTime++;
+			document.getElementById("measurementTime").innerText = currentMeasurementTime;
+		}
+	}, 1000);
+	
+	function updateLive(data) {
+		Object.keys(data).forEach((sensorID) => {
+			if (sensorID == "time") {
+				var date = new Date(data[sensorID]);
+				// Get time difference in seconds
+				var difference = ((Date.now() - date) / 1000).toFixed(0);
+				document.getElementById("measurementTime").innerText = difference;
+				currentMeasurementTime = difference;
+	
+				var statusEl = document.getElementById("systemStatus");
+				if (difference > 172800) {
+					statusEl.className = "text-danger";
+					statusEl.innerText = "Broken (greater than two days since last recording)";
+				} else if (difference > 3600) {
+					statusEl.className = "text-warning";
+					statusEl.innerText = "Problematic (greater than one hour since last recording)";
+				} else if (difference > 300) {
+					statusEl.className = "text-info";
+					statusEl.innerText = "Slow (greater than 5 minutes since last recording)";
+				} else {
+					statusEl.className = "text-success";
+					statusEl.innerText = "Fully operational";
+				}
+			} else {
+				var sensorEl = document.getElementById("live-" + sensorID);
+				if (sensorEl) {
+					var decimalPlaces = parseInt(sensorEl.dataset.decimal);
+					sensorEl.innerText = parseFloat(data[sensorID]).toFixed(decimalPlaces);
+				}
+			}
+		});
+	}
+
 	var pubnub;
 	try {
 		pubnub = new PubNub({
@@ -69,6 +69,26 @@ window.addEventListener("load", function(event) {
 				var data = response.messages.map(val => val.entry);
 				var latestData = data[data.length - 1];
 				updateLive(latestData);
+
+				var graphData = {};
+				data.forEach((currData) => {
+					var time = new Date(currData.time);
+					Object.keys(currData).forEach((sensorID) => {
+						if (sensorID == "time") return;
+						if (!graphData[sensorID]) {
+							graphData[sensorID] = [];
+						}
+						graphData[sensorID].push({
+							x: time,
+							y: currData[sensorID]
+						});
+					});
+				});
+
+				Object.keys(charts).forEach((sensorID) => {
+					charts[sensorID].chart.data.datasets[0].data = graphData[sensorID];
+					charts[sensorID].chart.update();
+				});
 			}
 		});
 		pubnub.addListener({
@@ -88,67 +108,56 @@ window.addEventListener("load", function(event) {
 			channels: ["measurement"] 
 		});
 	}
-	window.pubnub = pubnub;
 
 	// Set font stack
 	Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 	
-	var createGraphs = function (ajaxdata) {
-		for (var i = 0; i < ajaxdata.metadata.length; i++) {
-			var meta = ajaxdata.metadata[i];
-			meta.unit = meta.unit.replace(/&#176;/g, String.fromCharCode(176)); // fix degree symbol
-			new Chart(document.getElementById("graph-" + meta.sensorID).getContext("2d"), {
-				type: 'line',
-				data: {
-					labels: ["-195", "-190", "-185", "-180", "-175", "-170", "-165", "-160", "-155", "-150", "-145", "-140", "-135", "-130", "-125", "-120", "-115", "-110", "-105", "-100", "-95", "-90", "-85", "-80", "-75", "-70", "-65", "-60", "-55", "-50", "-45", "-40", "-35", "-30", "-25", "-20", "-15", "-10", "-5", "0"],
-					datasets: [{
-						label: meta.measurement + " (" + meta.unit + ")",
-						backgroundColor: meta.color,
-						borderColor: meta.color,
-						data: ajaxdata.values[meta.sensorID],
-						fill: false
-					}]
+	var charts = {};
+	Object.keys(metadata).forEach((key) => {
+		metadata[key].unit = metadata[key].unit.replace(/&#176;/g, String.fromCharCode(176)); // fix degree symbol
+
+		charts[key] = new Chart(document.getElementById("graph-" + key).getContext("2d"), {
+			type: 'scatter',
+			data: {
+				datasets: [{
+					label: metadata[key].measurement + " (" + metadata[key].unit + ")",
+					backgroundColor: metadata[key].color,
+					borderColor: metadata[key].color,
+					data: [],
+					fill: false
+				}]
+			},
+			options: {
+				pointRadius: 0,
+				tooltips: {
+					mode: 'index',
+					intersect: false,
 				},
-				options: {
-					responsive: true,
-					pointRadius: 0,
-					tooltips: {
-						mode: 'index',
-						intersect: false,
-					},
-					hover: {
-						mode: 'nearest',
-						intersect: true
-					},
-					scales: {
-						xAxes: [{
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						type: 'time',
+						position: 'bottom',
+						time: {
+							tooltipFormat: "h:mm a"
+						}
+					}],
+					yAxes: [{
+						type: 'linear',
+						ticks: {
+							min: metadata[key].graphMin,
+							max: metadata[key].graphMax
+						},
+						scaleLabel: {
 							display: true,
-							// type: 'time',
-							distribution: 'series',
-							scaleLabel: {
-								display: true,
-								labelString: 'Time (Mins ago)'
-							}
-							/*
-							time: {
-                    						unit: 'minute'
-                					}
-							*/
-						}],
-						yAxes: [{
-							display: true,
-							ticks: {
-								min: meta.min,
-								max: meta.max
-							},
-							scaleLabel: {
-								display: true,
-								labelString: meta.measurement
-							}
-						}]
-					}
+							labelString: metadata[key].measurement
+						}
+					}]
 				}
-			});
-		}
-	};
+			}
+		});
+	});
 }, false);
